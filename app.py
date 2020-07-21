27bd6f654454241945fc41147aa3b2b16ddcec03
emunozlorenzo <emunozlorenzo@gmail.com>
2020-07-21 20:08:32 +0200

    Stocks

--- app.py
+++ app.py
@@ -9,9 +9,16 @@ import matplotlib.pyplot as plt
 #from nltk.corpus import stopwords
 #nltk.download('stopwords')
 import numpy as np
+import pandas as pd
 from PIL import Image
 import pickle
 import yfinance as yf
+import altair as alt
+import numpy as np
+
+from bokeh.io import output_file, show
+from bokeh.models import ColumnDataSource, HoverTool
+from bokeh.plotting import figure
 
 #### CSS Style ####
 with open("style.css") as f:
@@ -41,6 +48,9 @@ def show_news(news,key,newspaper,summarise):
         #webbrowser.open(news['link'])
     st.markdown('**[Link]('+news["link"]+')**')
     st.markdown('\n')
+
+def datetime_(x):
+    return np.array(x, dtype=np.datetime64)
 
 #### DATA ####
 # Load News
@@ -91,8 +101,50 @@ def main():
             period = st.selectbox(label='Select Period', options=periods, index=5, key='stock_select_box')
             company = yf.Ticker(dict_yf[banks])
             hist = company.history(period=period)
-            close = hist[['Close']]
-            st.line_chart(close)
+            close = hist[['Close','Volume']]
+            # Graphs
+            #st.line_chart(close[['Close']],height=200)
+            #c = alt.Chart(close.reset_index()).mark_line().encode(x='Date',y='Close',tooltip=['Date', 'Close'])
+            #st.altair_chart(c, use_container_width=True)
+
+            close['Date'] = pd.to_datetime(close.index, format='%Y-%m-%d')
+            close["DateString"] = close["Date"].dt.strftime("%Y-%m-%d")
+
+            source = ColumnDataSource(data={
+                'date'      : close['Date'],
+                'adj close' : close['Close'],
+                'volume'    : close['Volume'],
+            })
+
+            p = figure(plot_height=250, x_axis_type="datetime", tools="", toolbar_location=None,
+                    title=banks, sizing_mode="scale_width")
+            p.background_fill_color="#f5f5f5"
+            p.grid.grid_line_color="white"
+            p.xaxis.axis_label = 'Date'
+            p.yaxis.axis_label = 'Price'
+            p.axis.axis_line_color = None
+
+            p.line(x='date', y='adj close', line_width=2, color='#16D29B', source=source)
+
+            p.add_tools(HoverTool(
+                tooltips=[
+                    #( 'date',   '@date{F%}'            ),
+                    ( 'close',  '@{adj close}{0.00 a}€' ), # use @{ } for field names with spaces
+                    ( 'volume', '@volume{0.00 a}'      ),
+                ],
+
+                formatters={
+                    'date'      : 'datetime', # use 'datetime' formatter for 'date' field
+                    'adj close' : 'printf',   # use 'printf' formatter for 'adj close' field
+                                            # use default 'numeral' formatter for other fields
+                },
+
+                # display a tooltip whenever the cursor is vertically in line with a glyph
+                mode='vline'
+            ))
+
+
+            st.bokeh_chart(p, use_container_width=True)
 
     # Selectbox
     options = [i.capitalize() for i in list(data.keys())] + ['All']
